9ad47222872effe835188fc5f6580cb49a69b1e2
Lir Mimrod <94037737+lirmimrod@users.noreply.github.com>
2022-11-28 19:35:01 +0200

    new

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,64 +1,49 @@
-import React,{useEffect, useState} from 'react';
+
+import React, { useState } from 'react';
 import './SearchBar.css'
 
 const SearchBar = () => {
 
-    const [searchValue,setSearchValue] = useState('')
+    const [searchValue, setSearchValue] = useState('')
     const [productState, setProductState] = useState([]);
 
     const handleInputChange = (event) => {
         setSearchValue(event.target.value)
     }
-    const handleClearClikc = () => {
+    const handleClearClick = () => {
         setSearchValue('')
     }
 
-    const func = () => {
-        console.log(productState)
+    const handleSearchClick = async () => {
+        const data = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchValue}`)
+        const productsArray = await data.json()
+        const newProdArray = productsArray.items.map(product => product.volumeInfo.title)
+
+        setProductState(newProdArray)
     }
 
-    const handleSearchClick = (searchValue)=>{
-        
-        fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchValue}`)
-        .then((res)=>res.json)
-        .then((productsArray)=>{
-            const newProdArray = productsArray.items.map((product)=>{
-                return product.volumeInfo.title
-            })
-            setProductState(newProdArray)
-        })
+    const filterArray = productState.filter(product => {
+        return product.toLowerCase().includes(searchValue.toLowerCase());
+    })
+    const shouldDisplayButton = searchValue.length > 2
 
-      //const data = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchValue}`)
-       // const productsArray = await data.json()
-        //console.log(productsArray)
-
-        //    .then((res)=> res.json())
-      //      .then((productsArray)=>{
-            //    const newProdArray = productsArray.items.map( product => product.volumeInfo.title )
-             //   console.log(productsArray)
-              //  setProductState(newProdArray)
-            
-    }
-
-    //const filterArray = productState.filter( product => product.includes(searchValue))
-    const filterArray = productState.filter( (product) => 
-         return product.includes(searchValue))
-    const shouldDisplayButton = searchValue.length >= 3
-    
     return (
-    <div>
-         <input type="text" value={searchValue} onChange={handleInputChange} />
-         <button onClick={handleSearchClick}>Clear</button>
-         <button onClick={func}>func</button>
-         {
-            productState.map( item => {
-                item.includes(searchValue)
-                <div>{item}</div>
-            })
-         }
-        
-    </div>
+        <div>
+            <input type="text" value={searchValue} onChange={handleInputChange} />
+            <button onClick={handleClearClick}>Clear</button>
+
+            {shouldDisplayButton? <button onClick={handleSearchClick}>search</button> : null}
+            
+            <ul style={{color:'blue'}}>
+                {
+                    filterArray.map((item,i) => <li key={i} >{item}</li>)
+                }
+            </ul>
+
+        </div>
     )
 }
 export default SearchBar
+
+
 
